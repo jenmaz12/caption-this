3177bed71e187bcdacf684beffa1240c39bfe737
jenmaz12 <jenmaz12@gmail.com>
2019-04-06 11:06:33 -0400

    remove nav from landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Images from './pages/Images';
-import Nav from './components/NavBar';
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
         <Switch>
           <Route exact path='/' component={Landing} />
           <Route exact path='/images' component={Images} />

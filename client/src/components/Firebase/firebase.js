56cae951163b14b95dba4fa927e4a84be0dfd249
jenmaz12 <jenmaz12@gmail.com>
2019-04-15 19:48:00 -0400

    firebase config

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,14 +1,13 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 require("dotenv").config();
 
-
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+    apiKey: "10318573-0d69a875d",
+    authDomain: "AIzaSyDkgcnwM87zO3ZAw1D9GYpatAq_dWUM_I4",
+    databaseURL: "captionthis-auth.firebaseapp.com",
+    projectId: "https://pixafinder.firebaseio.com",
+    storageBucket: "pixafinder",
+    messagingSenderId: 4532345391321
   };
 
 firebase.initializeApp(config);
